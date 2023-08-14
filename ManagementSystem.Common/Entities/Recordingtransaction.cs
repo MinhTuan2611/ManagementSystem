c5bc90a08993003823ce91e3a2649870a8d3357d
TuanHo <hominhtuan2611@gmail.com>
2023-08-15 01:17:09 +0700

    create Repository layer for accounting api service, create table

---
+++ ManagementSystem.Common/Entities/Recordingtransaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Common.Entities
+{
+    public class Recordingtransaction : BaseEntity
+    {
+        public int Id { get; set; }
+        public string TypeOfVoucherCode { get; set; }
+        public string TypeOfVoucherName { get; set;}
+        public int DebitAccountId { get; set; }
+        public int CreditAccountId { get; set; }
+    }
+}
---
+++ ManagementSystem.Common/Entities/TypesOfAccounts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Common.Entities
+{
+    public class TypesOfAccounts : BaseEntity
+    {
+        [Key]
+        public int AccountId { get; set; }
+        public string AccountCode { get; set; }
+        public string AccountName { get; set; }
+        public int AccountParentId { get; set; }
+        public int IsLiability { get; set; }
+        public int LiabilityType { get; set; }
+        public int AccountRank { get;}
+    }
+}
