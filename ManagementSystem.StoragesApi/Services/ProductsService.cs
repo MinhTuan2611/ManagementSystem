d6c2a44efc92b05a038f36701cc665760e9ae8e9
Toan Be <toanbe.cntt@gmail.com>
2023-11-09 19:45:19 +0700

    Update logic generate XML

--- ManagementSystem.StoragesApi/Services/ProductsService.cs
+++ ManagementSystem.StoragesApi/Services/ProductsService.cs
@@ -472,6 +472,37 @@ namespace ManagementSystem.StoragesApi.Services
                 return null;
             }
         }
+
+        public List<ProductDetailResponseDto> ProductDetailByIdAndUnit(int productId, int unitId)
+        {
+
+            string query = string.Format(@"
+                    SELECT p.ProductId
+		                    ,p.ProductCode
+		                    ,p.Price
+		                    ,p.DefaultPurchasePrice
+		                    ,p.BarCode
+		                    ,p.Tax
+		                    ,c.CategoryName
+		                    ,u.UnitName
+                    FROM dbo.Products p
+                    JOIN dbo.Category c ON c.CategoryId = p.CategoryId
+                    JOIN dbo.ProductUnit pu ON pu.ProductId = p.ProductId
+                    JOIN dbo.Unit U ON U.UnitId = pu.UnitId
+                    WHERE p.ProductId = {0} AND U.UnitId = {1}
+                ", productId, unitId);
+
+            try
+            {
+                var result = _storageContext.ProductResponseDtos.FromSqlRaw(query).ToList();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
         // private function check part name is in product name
         private bool CheckAnimalPartInProductName(string productName, List<string> AniamlPartSplit)
         {
