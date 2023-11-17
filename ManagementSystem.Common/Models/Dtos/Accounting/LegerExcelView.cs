1c62ef116da9f5c77174eacbd03ef474891b403c
Toan Be <toanbe.cntt@gmail.com>
2023-11-17 18:33:42 +0700

    Add logic export file

---
+++ ManagementSystem.Common/Models/Dtos/Accounting/LegerExcelView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Common.Models.Dtos
+{
+    public class LegerExcelView
+    {
+        public DateTime TransactionDate { get; set; }
+        public string? DepositAccount { get; set; }
+        public string? CreditAccount { get; set; }
+        public int? DoccumentNumber { get; set; }
+        public string? DoccumentType { get; set; }
+        public int? Amount { get; set; }
+        public string LegerDescription { get; set; }
+    }
+}
--- ManagementSystem.MainApp/Controllers/LegerController.cs
+++ ManagementSystem.MainApp/Controllers/LegerController.cs
@@ -1,11 +1,9 @@
-﻿using ManagementSystem.Common.Entities;
-using ManagementSystem.Common.GenericModels;
+﻿using ManagementSystem.Common.GenericModels;
 using ManagementSystem.Common.Helpers;
 using ManagementSystem.Common.Models;
 using ManagementSystem.Common.Models.Dtos;
-using Microsoft.AspNetCore.Http;
+using ManagementSystem.MainApp.Utility;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ManagementSystem.MainApp.Controllers
 {
@@ -26,6 +24,44 @@ namespace ManagementSystem.MainApp.Controllers
                 return Ok(result);
 
             return StatusCode(StatusCodes.Status404NotFound, "The list is empty");
+        }
+
+        [HttpPost("export_excel")]
+        public async Task<IActionResult> ExportExcelFile([FromBody] SearchCriteria searchModel)
+        {
+            var result = await HttpRequestsHelper.Post<ResponseDto>(SD.AccountingApiUrl + "Leger/export_excel", searchModel);
+
+            if (result.IsSuccess == false)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result);
+            }
+
+            string filePath = result.Result.ToString();
+            // Set the content type based on the file type
+            string contentType = "application/octet-stream";
+
+            // Set the file name displayed in the download dialog
+            string fileName = Path.GetFileName(filePath);
+
+
+            // Create a FileStreamResult with the file stream
+            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+
+            var response = File(fileStream, contentType, fileName);
+
+            // Register a callback to close the stream after the response is sent
+            Response.OnCompleted(() =>
+            {
+                fileStream.Dispose();
+
+                // Delete the file after it has been downloaded
+                System.IO.File.Delete(filePath);
+
+                return Task.CompletedTask;
+            });
+
+            return response;
         }
     }
 }
