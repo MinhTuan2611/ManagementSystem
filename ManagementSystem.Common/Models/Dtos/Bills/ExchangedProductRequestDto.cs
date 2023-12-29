c39a0556ed56f159caca2fe9de9dc4286f1704cd
LinhDao <daolinh1998k@gmail.com>
2023-12-29 17:14:46 +0700

    update logic refund

--- ManagementSystem.Common/Models/Dtos/Bills/ExchangedProductRequestDto.cs
+++ ManagementSystem.Common/Models/Dtos/Bills/ExchangedProductRequestDto.cs
@@ -8,14 +8,14 @@ namespace ManagementSystem.Common.Models.Dtos
 {
     public class ExchangedProductRequestDto
     {
-        public int? DocummentNumber { get; set; } = 1;
+        public int? DocummentNumber { get; set; }
         public int ProductId { get; set; }
         public int Price { get; set; } = 0;
         public int Quantity { get; set; } = 0;
         public int? Amount { get; set; } = 0;
         public int UnitId { get; set; }
-        public int? TotalMoneyAfterTax { get; set; } = 0;
+        public int? TotalMoneyAfterTax { get; set; }
         public string? Note { get; set; } = "";
-        public int? PaymentDiscountMoney { get; set; } = 0;
+        public int? PaymentDiscountMoney { get; set; }
     }
 }
