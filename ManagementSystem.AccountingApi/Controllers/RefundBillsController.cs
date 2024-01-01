49956fb0c2630b4e2d522d9959cd8925318907af
LinhDao <daolinh1998k@gmail.com>
2024-01-01 15:36:08 +0700

    update refundBill

--- ManagementSystem.AccountingApi/Controllers/RefundBillsController.cs
+++ ManagementSystem.AccountingApi/Controllers/RefundBillsController.cs
@@ -75,7 +75,6 @@ namespace ManagementSystem.AccountingApi.Controllers
                     TotalMoneyVND = model.TotalDeductibleAmount,
                     UserId = model.UserId,
                     ShiftId = 1,
-                    NTMoney = model.TotalDeductibleAmount
                 };
                 await _paymentVoucherService.CreatePaymentVoucher(paymentModel);
             }
